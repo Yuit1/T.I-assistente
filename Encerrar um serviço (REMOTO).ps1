17142c1cb3ede94ef702d9d686cb10921d2bccc5
Thiago Yuiti Miyata de Carvalho <97604343+Yuit1@users.noreply.github.com>
2023-12-31 13:45:26 -0300

    Encerrar um serviço (REMOTO).ps1

--- Encerrar um serviço (REMOTO).ps1
+++ Encerrar um serviço (REMOTO).ps1
@@ -1,33 +1,46 @@
-Write-Host "Você escolheu a opção 8. Ação 8 será executada."
-                start-Sleep -Seconds 2
-                cls
 
-                # Obter a lista de serviços em execução
-                $listaServicos = Get-Service | Where-Object { $_.Status -eq 'Running' }
+             $comeco = Read-Host "Você irá executar os comandos em qual HostName?"
 
-                # Exibir a lista de serviços
-                Write-Host "Serviços em execução:"
-                foreach ($servico in $listaServicos) {
-                    Write-Host "$($servico.DisplayName) (Nome do Serviço: $($servico.ServiceName))"
-                }
+try {
+    $result = Invoke-Command -ComputerName $comeco -ScriptBlock {
+        # Obter a lista de serviços em execução
+        $listaServicos = Get-Service | Where-Object { $_.Status -eq 'Running' }
 
-                # Perguntar ao usuário se deseja encerrar um serviço
-                $nomeServicoEncerrar = Read-Host "Digite o nome do serviço que você deseja encerrar (ou pressione Enter para sair)"
+        # Exibir a lista de serviços
+        Write-Host "Serviços em execução:"
+        foreach ($servico in $listaServicos) {
+            Write-Host "$($servico.DisplayName) (Nome do Serviço: $($servico.ServiceName))"
+        }
 
-                if ($nomeServicoEncerrar -ne "") {
-                    # Tentar encontrar o serviço com o nome fornecido pelo usuário
-                    $servicoEncerrar = $listaServicos | Where-Object { $_.DisplayName -eq $nomeServicoEncerrar -or $_.ServiceName -eq $nomeServicoEncerrar }
+        # Perguntar ao usuário se deseja encerrar um serviço
+        $nomeServicoEncerrar = Read-Host "Digite o nome do serviço que você deseja encerrar (ou pressione Enter para sair)"
 
-                    if ($servicoEncerrar) {
-                        # Encerrar o serviço
-                        Stop-Service -Name $servicoEncerrar.ServiceName
-                        Write-Host "O serviço $($servicoEncerrar.DisplayName) foi encerrado."
-                    } else {
-                        Write-Host "Nenhum serviço correspondente ao nome fornecido foi encontrado."
-                    }
-                } else {
-                    Write-Host "Você optou por não encerrar nenhum serviço."
-                }
+        if ($nomeServicoEncerrar -ne "") {
+            # Tentar encontrar o serviço com o nome fornecido pelo usuário
+            $servicoEncerrar = $listaServicos | Where-Object { $_.DisplayName -eq $nomeServicoEncerrar -or $_.ServiceName -eq $nomeServicoEncerrar }
 
-                pause
-                cls
+            if ($servicoEncerrar) {
+                # Encerrar o serviço
+                Stop-Service -Name $servicoEncerrar.ServiceName
+                Write-Host "O serviço $($servicoEncerrar.DisplayName) foi encerrado."
+            } else {
+                Write-Host "Nenhum serviço correspondente ao nome fornecido foi encontrado."
+            }
+        } else {
+            Write-Host "Você optou por não encerrar nenhum serviço."
+        }
+    }
+
+    if ($result -eq $null) {
+        Write-Host "Verifique a conexão no Host $comeco." -ForegroundColor Yellow
+    } else {
+        Write-Host "Comandos executados com sucesso no Host $comeco." -ForegroundColor Green
+        Write-Host $result
+    }
+} catch {
+    Write-Host "Erro ao conectar-se ao host $comeco. Verifique o nome do host e a conectividade." -ForegroundColor Red
+} finally {
+    # Pausa para visualização
+    pause
+    Clear-Host
+}
